9050fd693953a032ef8131df916ab985213dc0d3
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-02-26 16:44:12 +0100

    Update DataDumpResult.java

--- src/main/java/co/nyzo/verifier/DataDumpResult.java
+++ src/main/java/co/nyzo/verifier/DataDumpResult.java
@@ -1,11 +1,13 @@
 package co.nyzo.verifier;
 
+
 public class DataDumpResult {
     public long generatedTimestamp = -1;
     public long creatorInitialisationTimestamp = -1;
     public String systemMessage = "";
     public String resultMessage = "";
 
+    public DataDumpResultNodeInfo nodeInfo = null;
     public Object result = null;
 
     public DataDumpResult(Object data, String msg, long initialisationTimestamp) {
@@ -13,5 +15,6 @@ public class DataDumpResult {
         result = data;
         resultMessage = msg;
         creatorInitialisationTimestamp = initialisationTimestamp;
+        nodeInfo = new DataDumpResultNodeInfo();
     }
 }
