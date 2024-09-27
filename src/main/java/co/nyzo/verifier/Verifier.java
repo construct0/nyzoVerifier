6bd47b1c558b2206770a9f88ebeb4a1281ddff91
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-09-27 02:20:06 +0200

    Version 645.4 - Remove pathway overhead, remove unused version request functionality from NodeManager
    Update Version.java

--- src/main/java/co/nyzo/verifier/Verifier.java
+++ src/main/java/co/nyzo/verifier/Verifier.java
@@ -333,7 +333,7 @@ public class Verifier {
                 buffer.rewind();
                 genesisBlock = Block.fromByteBuffer(buffer);
 
-                LogUtil.println("[Verifier][loadGenesisBlock]: fetched block: " + genesisBlock.toString());
+                LogUtil.println("[Verifier][loadGenesisBlock]: fetched block: " + genesisBlock.toStringVerbose());
 
             } catch (Exception e) {
                 LogUtil.println("[Verifier][loadGenesisBlock]: " + e.toString() + ",\r\n" + e.getStackTrace().toString());
